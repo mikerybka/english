e694d252fa5c3401df1696a006766f776c7ce262
Mike Rybka <merybka@gmail.com>
2024-01-29 15:42:15 -0500

    Create acronoyms.go file

---
+++ acronyms.go
@@ -0,0 +1,40 @@
+package english
+
+var twoLetterAcronyms = map[string]bool{
+	"ID": true,
+	"IP": true,
+	"IO": true,
+	"OS": true,
+	"UI": true,
+	"UX": true,
+}
+
+var threeLetterAcronyms = map[string]bool{
+	"API":  true,
+	"CPU":  true,
+	"CSS":  true,
+	"DNS":  true,
+	"EOF":  true,
+	"GUI":  true,
+	"HTML": true,
+	"JSON": true,
+	"OOP":  true,
+	"RPC":  true,
+	"SSL":  true,
+	"TCP":  true,
+	"TLS":  true,
+	"UDP":  true,
+	"URI":  true,
+	"URL":  true,
+	"UTF":  true,
+	"WWW":  true,
+	"XML":  true,
+}
+
+var fourLetterAcronyms = map[string]bool{
+	"HTTP": true,
+}
+
+var fiveLetterAcronyms = map[string]bool{
+	"HTTPS": true,
+}
